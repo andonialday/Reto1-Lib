6e9d096bb885f38229d48b0918593d4e4a1f75fd
enaizabil <enaizabil@gmail.com>
2021-10-28 09:57:44 +0200

    Clases DBCreateException y DBReadException Preparadas.

--- src/reto1libraries/exception/DBCreateException.java
+++ src/reto1libraries/exception/DBCreateException.java
@@ -1,22 +1,24 @@
 package reto1libraries.exception;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Custom controller for Create type SQL Exceptions
  * @author Enaitz Izagirre
  */
 public class DBCreateException extends Exception{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	
+    private static final long serialVersionUID = 1L;
 
     /**
      * Generation of the customized Exception
-     * @param message
+     * @param message show message to the user
      */
     public DBCreateException(String message) {
-		super(message);
+	Logger logger = Logger.getAnonymousLogger();
+        logger.log(Level.SEVERE, message, this);
 	}
 
 	
--- src/reto1libraries/exception/DBReadException.java
+++ src/reto1libraries/exception/DBReadException.java
@@ -1,22 +1,24 @@
 package reto1libraries.exception;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Custom controller for Read type SQL Exceptions
  * @author Enaitz Izagirre
  */
 public class DBReadException extends Exception{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+     
 
     /**
      * Generation of the customized Exception
-     * @param message
+     * @param message to show to the user
      */
     public DBReadException(String message) {
-		super(message);
+        Logger logger = Logger.getAnonymousLogger();
+        logger.log(Level.SEVERE, message, this);
 	}
 
 	
