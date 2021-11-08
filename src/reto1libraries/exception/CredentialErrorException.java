45f0a017f4f5cea491a65a96ca8b11b1bbfc3d7b
andonialday <andalday@gmail.com>
2021-11-08 08:10:36 +0100

    Error Fixing

---
+++ src/reto1libraries/exception/CredentialErrorException.java
@@ -0,0 +1,28 @@
+package reto1libraries.exception;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Custom trigger for credential error on SignIn requests
+ *
+ * @author Andoni Alday
+ */
+public class CredentialErrorException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Generation of the customized Exception
+     *
+     * @param message
+     */
+    public CredentialErrorException(String message) {
+        Logger logger = Logger.getAnonymousLogger();
+        logger.log(Level.SEVERE, message, this);
+    }
+
+}
